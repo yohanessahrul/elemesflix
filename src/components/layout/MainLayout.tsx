d5b30fcb80c6258261bd31a7dbf255cbf16e6cb3
yohanessahrul <yohanessahrul92@gmail.com>
2026-09-03 00:47:57 +0700

    fix: update responsive main layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -2,7 +2,7 @@ import AppRouter from "../../routes/AppRouter";
 
 export default function MainLayout() {
   return (
-    <div className="w-full pt-25 px-12.5 pb-12.5">
+    <div className="w-full pt-25 px-4 sm:px-4 md:px-8 lg:px-12 pb-12.5">
       <AppRouter /> 
     </div>
   );
