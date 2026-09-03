5bbd1ab04f56192827a60823917d22065433adc4
yohanessahrul <yohanessahrul92@gmail.com>
2026-09-03 14:35:54 +0700

    feat: add detail poster

--- src/components/common/PosterCard.tsx
+++ src/components/common/PosterCard.tsx
@@ -31,26 +31,43 @@ export default function PosterCard(props: PosterCardType) {
       }
       
       {props.isHasCTA && (
-        <div className="absolute top-0 left-0 w-full h-full z-50  flex justify-center items-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-          <button
-            className={`${isSuccessAdd ? 'bg-green-500': 'bg-amber-300'} px-4 py-2 rounded-md cursor-pointer flex text-md`}
-            onClick={() => addToWatchlistHandler(props.item)}
-          >
-            {isSuccessAdd ? <Check /> : <Plus />} &nbsp; {isSuccessAdd ? "Success" : "Watchlist"}
-          </button>
+        <div className="absolute bottom-0 left-0 w-full z-50  flex justify-center items-end transition-opacity duration-300 opacity-0 group-hover:opacity-100 bg-linear-to-t from-black to-transparent">
+          <div className='flex flex-col'>
+            <div className='flex justify-center items-center my-4'>
+              <button
+                className={`${isSuccessAdd ? 'bg-green-500': 'bg-amber-300'} px-4 py-2 rounded-md cursor-pointer flex text-md`}
+                onClick={() => addToWatchlistHandler(props.item)}
+              >
+                {isSuccessAdd ? <Check /> : <Plus />} &nbsp; {isSuccessAdd ? "Success" : "Watchlist"}
+              </button>
+            </div>
+            <div className='text-center py-4'>
+              <h2 className='text-white font-bold text-lg px-4'>{props.item.original_name ? props.item.original_name : props.item.original_title}</h2>
+              <p className='text-yellow-400'>Rating : {props.item.vote_average}</p>
+            </div>
+          </div>
         </div>
       )}
 
       {!props.isHasCTA && (
-        <div className="absolute top-0 left-0 w-full h-full z-50  flex justify-center items-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-          <button
-            className="bg-red-600 text-white px-4 py-2 rounded-md cursor-pointer flex text-md"
-            onClick={() => {
-              removeOnWatchlist(props.item.id)
-            }}
-          >
-            <Trash2 /> &nbsp; Remove
-          </button>
+        <div className="absolute bottom-0 left-0 w-full z-50  flex justify-center items-center transition-opacity duration-300 opacity-0 group-hover:opacity-100 bg-linear-to-t from-black to-transparent">
+          <div className='flex flex-col'>
+
+            <div className='flex justify-center items-center my-4'>
+              <button
+                className="bg-red-600 text-white px-4 py-2 rounded-md cursor-pointer flex text-md"
+                onClick={() => {
+                  removeOnWatchlist(props.item.id)
+                }}
+              >
+                <Trash2 /> &nbsp; Remove
+              </button>
+            </div>
+            <div className='text-center py-4'>
+              <h2 className='text-white font-bold text-lg px-4'>{props.item.original_name ? props.item.original_name : props.item.original_title}</h2>
+              <p className='text-yellow-400'>Rating : {props.item.vote_average}</p>
+            </div>
+          </div>
         </div>
       )}
     </div>
--- src/types/movies.ts
+++ src/types/movies.ts
@@ -6,6 +6,7 @@ export interface Movie {
   title: string;
   original_language: string;
   original_title: string;
+  original_name?: string;
   overview: string;
   popularity: number;
   poster_path: string | null;
