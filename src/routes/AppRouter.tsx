30184b3d66c131152596a23e39399beee3a32d38
yohanessahrul <yohanessahrul92@gmail.com>
2026-09-03 11:07:59 +0700

    fix: clear unused component

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import MoviesPage from "../pages/Movies";
 import WatchListPage from "../pages/WatchList";
